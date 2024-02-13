6a04ef7e680dcb8797e35b639e48b95b33c3faa8
zakouril.stepan <zakouril.stepan@ssakhk.cz>
2024-02-13 14:37:42 +0100

    Client update

--- RobotaNearMe/Data/ApplicationDbContext.cs
+++ RobotaNearMe/Data/ApplicationDbContext.cs
@@ -5,9 +5,14 @@ namespace RobotaNearMe.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<JobOffer> JobOffers { get; set; }
+        public DbSet<PreviousJobs> PreviousJobs { get; set; }
+        public DbSet<User> User {  get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        
     }
 }
